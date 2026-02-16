151d47793008e63affb1a31620e40de940430489
Prathipan V <prathipan539@gmail.com>
2026-02-16 17:54:06 +0530

    files updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='prathipan',
     author_email='prathipan539@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["langchain-google-genai","google-generativeai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
